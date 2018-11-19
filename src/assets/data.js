54015d4de05e0cf93a7ffa958670ecb1b3244887
Luis <martinez.s.luis@gmail.com>
2018-11-19 10:42:45 +0100

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,7 @@
+let sliderArray = [
+   { img: 'http://bloquo.cc/img/works/1.jpg', alt: 'image1', text: 'Lorem ipsum dolor sit amet' },
+   { img: 'http://bloquo.cc/img/works/2.jpg', alt: 'image2', text: 'Consectetur adipiscing elit' },
+   { img: 'http://bloquo.cc/img/works/3.jpg', alt: 'image3', text: 'Sed do eiusmod tempor incididunt' },
+   { img: 'http://bloquo.cc/img/works/4.jpg', alt: 'image4', text: 'Ut labore et dolore magna aliqua' },
+   { img: 'http://bloquo.cc/img/works/5.jpg', alt: 'image5', text: 'Ut enim ad minim veniam' }
+];
