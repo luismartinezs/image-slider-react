1f15891cce1de22a4744727d4f1ad71ad5075075
Luis Martinez <martinez.s.luis@gmail.com>
2018-11-21 19:33:47 +0100

    Updates

---
+++ precache-manifest.ae25420d531fcf3e1fee68491d609f3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c11ab94a8604d754cf8",
+    "url": "/image-slider-react/static/js/runtime~main.0c11ab94.js"
+  },
+  {
+    "revision": "a69b282d38f83e8810d9",
+    "url": "/image-slider-react/static/js/main.a69b282d.chunk.js"
+  },
+  {
+    "revision": "09fc55047213e3a1f780",
+    "url": "/image-slider-react/static/js/1.09fc5504.chunk.js"
+  },
+  {
+    "revision": "a69b282d38f83e8810d9",
+    "url": "/image-slider-react/static/css/main.a9ad031a.chunk.css"
+  },
+  {
+    "revision": "6ef9a3d2db1655e5811c57eeed5f787d",
+    "url": "/image-slider-react/index.html"
+  }
+];
